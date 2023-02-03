c1e3c3cc8cc454b1591b602b5579f86cab258f42
arvindk07 <info.arvind07@gmail.com>
2023-02-03 11:56:45 +0530

    updated function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,29 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import todo from "../assets/images/todo.svg";
+
+const getLocalItems = () => {
+  let list = localStorage.getItem("lists");
+  console.log(list);
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
+    return [];
+  }
+};
 
 const Todo = () => {
   const [inputData, setInputData] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalItems());
+  const [toggleSubmit, setToggleSUbmit] = useState(true);
+  const [isEditItem, setIsEditItem] = useState(null);
 
   const addItems = () => {
     if (!inputData) {
+      alert("Please Fill Data");
+    } else if (inputData && !toggleSubmit) {
+      setItems(
+        items.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, name: inputData };
+          }
+          return elem;
+        })
+      );
+      setToggleSUbmit(true);
+
+      setInputData("");
+
+      setIsEditItem(null);
     } else {
-      setItems([...items, inputData]);
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
       setInputData("");
     }
   };
 
-  const deleteItem = (id) => {
-    console.log(id);
-    const updatedItems = items.filter((elem, index) => {
-      return index !== id;
+  const deleteItem = (index) => {
+    // console.log(id);
+    const updatedItems = items.filter((elem) => {
+      return index !== elem.id;
     });
     setItems(updatedItems);
+  };
+
+  // edit Item
+  // when user click on edit button
+  // ===>>>>>>>>>>>>
+  // 1: get the id and name of the data which user clicked to edit
+  // 2: set the toggle mode to change the submit button into edit button
+  // 3: now update the value of the setInput with the new updated value to the edit
+  // 4: to pass current element id to new state variable for refernce
+
+  const editItem = (id) => {
+    let newEditItem = items.find((elem) => {
+      return elem.id === id;
+    });
+    console.log(newEditItem);
+
+    setToggleSUbmit(false);
+
+    setInputData(newEditItem.name);
+
+    setIsEditItem(id);
   };
 
   const removeAll = () => {
     setItems([]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(items));
+  }, [items]);
   return (
     <>
       <div className="mainDiv">
@@ -39,22 +95,37 @@ const Todo = () => {
               value={inputData}
               onChange={(e) => setInputData(e.target.value)}
             />
-            <i
-              className="fa fa-plus add-btn"
-              title="Add Item"
-              onClick={addItems}
-            ></i>
+            {toggleSubmit ? (
+              <i
+                className="fa fa-plus add-btn"
+                title="Add Item"
+                onClick={addItems}
+              ></i>
+            ) : (
+              <i
+                className="far fa-edit add-btn"
+                title="Update Item"
+                onClick={addItems}
+              ></i>
+            )}
           </div>
           <div className="showItems">
-            {items.map((elem, index) => {
+            {items.map((elem) => {
               return (
-                <div className="eachItem" key={index}>
-                  <h3>{elem}</h3>
-                  <i
-                    className=" far fa-trash-alt add-btn"
-                    title="Delete Item"
-                    onClick={() => deleteItem(index)}
-                  ></i>
+                <div className="eachItem" key={elem.id}>
+                  <h3>{elem.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className=" far fa-edit add-btn"
+                      title="Edit Item"
+                      onClick={() => editItem(elem.id)}
+                    ></i>
+                    <i
+                      className=" far fa-trash-alt add-btn"
+                      title="Delete Item"
+                      onClick={() => deleteItem(elem.id)}
+                    ></i>
+                  </div>
                 </div>
               );
             })}
